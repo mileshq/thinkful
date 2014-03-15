b746be2584115c2bc2ad0837395340b8bee1b253
mileshq <mileshq@gmail.com>
2014-03-15 13:04:16 -0400

    add first test

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -1,2 +1,9 @@
-def apply_discount(cart_total, discount, discount_type):
-	pass
+
+class DiscountCalculator1(object):
+	def calculate_discount(cart_total, discount, discount_type):
+		if discount_type == "percent" or discount_type == "%":
+			return cart_total * (discount / 100)
+		elif discount_type == "dollar" or discount_type == "$":
+			return cart_total - discount
+		else:
+			raise ValueError
